ebe71268dc77cc59bb1dc4bdccdf62c149a1400b
Searge <searge@duck.com>
2024-12-02 21:13:33 +0200

    feat: Add example to the code
    Examples of how to use our code, which are checked as part of our tests
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/integers

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	t.Log("Testing Add function with inputs 2 and 2")
@@ -12,4 +15,10 @@ func TestAdder(t *testing.T) {
 	} else {
 		t.Logf("Success: expected '%d' and got '%d'", expected, sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
