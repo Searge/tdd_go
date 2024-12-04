c0c388d839b14caa005974ca944e8c1fed5e4edb
Searge <searge@duck.com>
2024-12-04 14:59:43 +0200

    feat: Implement SumAll funct & test

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,23 @@
 package arrays
 
+// Sum takes a slice of integers and returns the sum of all the numbers in the slice.
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll takes in a variadic number of slices of integers and returns a slice of
+// the sums of each slice.
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,7 +1,14 @@
 package arrays
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
+// TestSum tests the Sum function to ensure it correctly returns the sum of
+// all the numbers in the given slice of integers. It checks that the result
+// matches the expected output for two different input slices.
 func TestSum(t *testing.T) {
 
 	t.Run("collection of 5 numbes", func(t *testing.T) {
@@ -24,4 +31,24 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+// TestSumAll tests the SumAll function to ensure it correctly returns a slice
+// containing the sums of each provided slice of integers. It checks that the
+// result matches the expected output for given input slices.
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func ExampleSumAll() {
+	sums := SumAll([]int{1, 2}, []int{0, 9})
+	fmt.Println(sums)
+	// Output:
+	// [3 9]
 }
