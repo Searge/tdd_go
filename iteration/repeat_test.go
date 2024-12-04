8d59563ff40d9bbd3fcd8ceb8db9e492726214e9
Searge <searge@duck.com>
2024-12-04 10:33:22 +0200

    feat: Add more test & refactor Repeat func

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -5,23 +5,37 @@ import (
 	"testing"
 )
 
-func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+const repeatCount = 5
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+func TestRepeat(t *testing.T) {
+	t.Run("repeat character 5 times", func(t *testing.T) {
+		repeated := Repeat("a", repeatCount)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	// Test with zero times
+	t.Run("repeat character 0 times", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := ""
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", repeatCount)
 	}
 }
 
 func ExampleRepeat() {
-	repeated := Repeat("a")
+	repeated := Repeat("a", repeatCount)
 	fmt.Println(repeated)
 	// Output: aaaaa
 }
