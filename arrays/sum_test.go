d3777647dd5f06a29168f0c1bd688e5e389de4dc
Searge <searge@duck.com>
2024-12-04 18:34:08 +0200

    feat: Implement SumAllTails

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -52,3 +52,12 @@ func ExampleSumAll() {
 	// Output:
 	// [3 9]
 }
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
