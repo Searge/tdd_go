0a182ea22d3bcbc9d7298024fdc98cefbd810d35
Searge <searge@duck.com>
2024-12-08 00:17:03 +0200

    chore: Update dictionary search error handling
    Changes the dictionary search function to return a custom error instead of a generic error message when the searched word is not found.
    
    This makes the error message more informative and allows for better error handling in the calling code.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,17 +1,18 @@
 package maps
 
-import "fmt"
+import "errors"
 
 type Dictionary map[string]string
 
-// Search looks up a word in the dictionary and returns its definition.
-// If the word is not present in the dictionary, it returns an empty string.
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
 	// The second value is a boolean which indicates if the key was found successfully.
 	definition, ok := d[word]
 
 	if !ok {
-		return "", fmt.Errorf("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
+
 	return definition, nil
 }
