57e82909755afb444a50232d3b3f5acd2746ba57
Searge <searge@duck.com>
2024-12-07 22:10:26 +0200

    feat: Adds error handling to wallet withdrawal functionality
    Modifies the `Wallet.Withdraw` method to return an error if the requested withdrawal amount exceeds the available balance.
    This ensures that the wallet's balance is not decremented when there are insufficient funds, preventing potential errors or inconsistencies.
    
    Additionally, the commit adds a new test case to validate the error handling for withdrawals with insufficient funds.

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -4,33 +4,53 @@ import (
 	"testing"
 )
 
+// TestWallet tests the Wallet struct and its methods.
+//
+// It contains four tests:
+//
+//  1. Deposit: Test that the balance of the wallet is increased by the amount
+//     deposited.
+//  2. Withdraw: Test that the balance of the wallet is decreased by the amount
+//     withdrawn.
+//  3. Withdraw insufficient funds: Test that the balance of the wallet is not
+//     decreased by the amount withdrawn when there are not enough funds in the
+//     wallet.
 func TestWallet(t *testing.T) {
 
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
+	})
+
 }
