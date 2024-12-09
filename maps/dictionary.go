9ef5afcab72f5a3c42adbb2e82784c4a0c9505af
Searge <searge@duck.com>
2024-12-09 11:29:08 +0200

    feat: Adds error handling for updating and deleting words in the dictionary
    Adds a new error `ErrWordDoesNotExist` to indicate that a word does not exist in the dictionary, which can occur when attempting to update or delete a word.
    
    Implements the `Update` method to update the definition of an existing word in the dictionary. If the word does not exist, it returns the `ErrWordDoesNotExist` error.
    
    Implements the `Delete` method to remove a word from the dictionary.
    
    Adds new test cases to cover the `Update` and `Delete` functionality.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,8 +6,9 @@ type Dictionary map[string]string
 // Predefined errors
 // An article: https://dave.cheney.net/2016/04/07/constant-errors
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
 )
 
 type DictionaryErr string
@@ -47,4 +48,28 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 
 	return nil
+}
+
+// Update updates the definition of an existing word in the dictionary.
+//
+// If the word does not exist in the dictionary, it returns ErrWordDoesNotExist.
+// If the word already exists, it updates the definition.
+// If an error occurs while searching for the word, it is returned.
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -44,6 +44,44 @@ func TestAdd(t *testing.T) {
 
 }
 
+// TestUpdate tests the Update method of the Dictionary type.
+// It checks that updating the definition of an existing word
+// in the dictionary succeeds, and that attempting to update
+// a non-existent word returns an ErrWordDoesNotExist error.
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	assertError(t, err, ErrNotFound)
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
