76b0abe2ef59f2d26e01746f14f19c6f62cb8631
Searge <searge@duck.com>
2024-12-09 11:19:51 +0200

    feat: Implement Constant errors

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,14 +1,25 @@
+// Package maps provides a simple dictionary implementation.
 package maps
-
-import "errors"
 
 type Dictionary map[string]string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+// Predefined errors
+// An article: https://dave.cheney.net/2016/04/07/constant-errors
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
+type DictionaryErr string
+
+// Error satisfies the error interface and returns the string value of e.
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Search looks up a word in the dictionary and returns its definition.
+// If the word is found, it returns the definition and a nil error.
+// If the word is not found, it returns an empty string and ErrNotFound.
 func (d Dictionary) Search(word string) (string, error) {
 	// The second value is a boolean which indicates if the key was found successfully.
 	definition, ok := d[word]
@@ -20,8 +31,20 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
+// Add a word and its definition to the dictionary.
+//
+// Returns ErrWordExists if the word already exists in the dictionary.
 func (d Dictionary) Add(word, definition string) error {
-	d[word] = definition
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
 
 	return nil
 }
