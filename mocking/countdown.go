574b81d22bc3c9dd660b5178897a793d8a91df34
Searge <searge@duck.com>
2024-12-13 08:06:07 +0200

    refactor: Add constats and sleep timer

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,12 +3,19 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
 )
 
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 
-	fmt.Fprintln(out, "Go!")
+	fmt.Fprintln(out, finalWord)
 }
