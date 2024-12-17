8d493575633e3c3e2aeabeac15e18a896aa97a92
Searge <searge@duck.com>
2024-12-17 16:42:40 +0200

    chore: Improve mocking and testing for countdown functionality
    Introduces a new `ConfigurableSleeper` type that allows for customizable sleep durations and provides a `SpyTime` type to track the duration of sleep calls.
    
    Adds additional tests to ensure the countdown function correctly sleeps before each print and verifies the expected countdown output.

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -17,6 +17,23 @@ type Sleeper interface {
 
 type DefaultSleeper struct{}
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 type SpyCountdownOperations struct {
 	Calls []string
 }
